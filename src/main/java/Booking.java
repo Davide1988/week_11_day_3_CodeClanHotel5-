dcee3efaf1b2ce78b71cde5bd56a0d583ab9490a
Davide Venditti <davide_335@msn.com>
2019-02-13 14:38:07 +0000

    Adds Booking class with passing tests.

---
+++ src/main/java/Booking.java
@@ -0,0 +1,26 @@
+public class Booking {
+
+    private BedRoom bedRoom;
+    private int noOfNights;
+    private int noOfClients;
+
+    public Booking(BedRoom bedRoom, int noOfNights, int noOfClients){
+        this.bedRoom = bedRoom;
+        this.noOfNights = noOfNights;
+        this.noOfClients = noOfClients;
+    }
+
+
+    public BedRoom getBedroom(){
+        return this.bedRoom;
+    }
+
+    public int getNoOfNights(){
+        return this.noOfNights;
+    }
+
+    public int getNoOfClients(){
+        return this.noOfClients;
+    }
+
+}
---
+++ src/test/java/BookingTest.java
@@ -0,0 +1,34 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BookingTest {
+
+    private  Booking booking;
+    private BedRoom bedRoom1;
+
+    @Before
+    public void before(){
+
+        booking = new Booking(bedRoom1, 2, 2);
+
+    }
+
+
+    @Test
+    public void canGetBedroom(){
+        assertEquals(bedRoom1, booking.getBedroom());
+    }
+
+    @Test
+    public void canGetNoOfNights(){
+        assertEquals(2, booking.getNoOfNights());
+    }
+
+    @Test
+    public void canGetNoOfClients(){
+        assertEquals(2, booking.getNoOfClients());
+    }
+
+}
